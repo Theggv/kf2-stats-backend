ea8303a339fd37a32d41cda098c59caf7dc78ad7
Theggv <m11rrrrr123@gmail.com>
2023-10-22 21:33:46 +0500

    Added godotenv

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.2
+	github.com/joho/godotenv v1.5.1 
 )
 
 require (
--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -1,17 +1,32 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 type AppConfig struct {
 	ServerAddr   string
 	DatabasePath string
 	Token        string
+	SteamApiKey  string
 }
 
 var Instance *AppConfig = new()
 
 func new() *AppConfig {
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Print(".env file is not located, skipping\n")
+	}
+
 	return &AppConfig{
 		ServerAddr:   getEnv("SERVER_ADDR", "127.0.0.1:3000"),
 		DatabasePath: getEnv("DB_PATH", "store.sqlite"),
 		Token:        getEnv("SECRET_TOKEN", ""),
+		SteamApiKey:  getEnv("STEAM_API_KEY", ""),
 	}
 }
